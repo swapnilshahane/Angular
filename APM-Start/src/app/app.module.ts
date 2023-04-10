c39c84a5780ed3ed84b37d4e9791415c2aaab5ed
swapnilshahane <s.p.shahane@gmail.com>
2023-04-10 18:55:22 +0530

    added product service and subscried to observable

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -16,7 +17,8 @@ import { StarComponent } from './shared/start.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
