8c149634503f787bdbe979ed57570476d4a9d858
swapnilshahane <s.p.shahane@gmail.com>
2023-04-17 16:51:13 +0530

    Added search functionality in product list

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -7,18 +7,30 @@ import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/start.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo:'welcome', pathMatch:'full'},
+      {path: '*', redirectTo: 'welcome', pathMatch:'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch:'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
